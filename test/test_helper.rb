904bf5536c2d36cc137b56efaadc3d2c277891ee
aleksacastle <aaklochko@gmail.com>
2017-06-18 13:22:48 +0000

    Add new file test_helper.rb

---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/unit'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require 'calculator'
+require 'variant3'
